6d4691bebd28d5d09a7d5a70d0d5b05ca5e41af7
Dedygans <dedyharahapchannel@gmail.com>
2023-04-14 19:44:43 +0700

    menghapus variable string

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -7,11 +7,10 @@ import java.util.List;
 
 public class Employee {
 
-	private String employeeId;
-	private String firstName;
-	private String lastName;
+
+
 	private String idNumber;
-	private String address;
+
 	
 	private int yearJoined;
 	private int monthJoined;
@@ -37,12 +36,10 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, jk gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
+	public Employee( String idNumber,  int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, jk gender) {
+		
 		this.idNumber = idNumber;
-		this.address = address;
+	
 		this.yearJoined = yearJoined;
 		this.monthJoined = monthJoined;
 		this.dayJoined = dayJoined;
